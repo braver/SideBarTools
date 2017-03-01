66cdf84f82c76a658e0a0cce35c1f06082eff76b
Koen Lageveen <koen.lageveen@me.com>
2017-03-01 19:58:12 +0100

    cleanup

--- SideBar.py
+++ SideBar.py
@@ -1,19 +1,13 @@
 # coding=utf8
 import sublime, sublime_plugin
 
-import os, shutil
-import threading, time
-import re
-
-try:
-	from urllib import unquote as urlunquote
-except ImportError:
-	from urllib.parse import unquote as urlunquote
+import os
+import threading
+import time
 
 from .SideBarAPI import SideBarItem, SideBarSelection, SideBarProject
 
-global Pref, s, Cache
-Pref = {}
+global s, Cache
 s = {}
 Cache = {}
 
@@ -23,36 +17,13 @@ def CACHED_SELECTION(paths = []):
 	else:
 		return SideBarSelection(paths)
 
-def escapeCMDWindows(string):
-	return string.replace('^', '^^')
-
-class Pref():
-	def load(self):
-		pass
-
-def plugin_loaded():
-	global Pref, s
-	s = sublime.load_settings('Side Bar.sublime-settings')
-	Pref = Pref()
-	Pref.load()
-	s.clear_on_change('reload')
-	s.add_on_change('reload', lambda:Pref.load())
-
 def Window(window = None):
 	return window if window else sublime.active_window()
-
-def expandVars(path):
-	for k, v in list(os.environ.items()):
-		path = path.replace('%'+k+'%', v).replace('%'+k.lower()+'%', v)
-	return path
 
 def window_set_status(key, name =''):
 	for window in sublime.windows():
 		for view in window.views():
 			view.set_status('SideBar-'+key, name)
-
-class Object():
-	pass
 
 class Cache():
 	pass
@@ -144,47 +115,6 @@ class SideBarCopyPathAbsoluteFromProjectCommand(sublime_plugin.WindowCommand):
 
 	def is_enabled(self, paths = []):
 		return CACHED_SELECTION(paths).len() > 0 and CACHED_SELECTION(paths).hasItemsUnderProject()
-
-class SideBarCopyUrlCommand(sublime_plugin.WindowCommand):
-	def run(self, paths = []):
-		items = []
-
-		for item in SideBarSelection(paths).getSelectedItems():
-			if item.isUnderCurrentProject():
-				items.append(item.url('url_production'))
-
-		if len(items) > 0:
-			sublime.set_clipboard("\n".join(items));
-			if len(items) > 1 :
-				sublime.status_message("Items URL copied")
-			else :
-				sublime.status_message("Item URL copied")
-
-	def is_enabled(self, paths = []):
-		return CACHED_SELECTION(paths).hasItemsUnderProject()
-
-class SideBarCopyUrlDecodedCommand(sublime_plugin.WindowCommand):
-	def run(self, paths = []):
-		items = []
-
-		for item in SideBarSelection(paths).getSelectedItems():
-			if item.isUnderCurrentProject():
-				txt = item.url('url_production')
-				try:
-					txt = urlunquote(txt.encode('utf8')).decode('utf8')
-				except TypeError:
-					txt = urlunquote(txt)
-				items.append(txt)
-
-		if len(items) > 0:
-			sublime.set_clipboard("\n".join(items));
-			if len(items) > 1 :
-				sublime.status_message("Items URL copied")
-			else :
-				sublime.status_message("Item URL copied")
-
-	def is_enabled(self, paths = []):
-		return CACHED_SELECTION(paths).hasItemsUnderProject()
 
 class SideBarDuplicateCommand(sublime_plugin.WindowCommand):
 	def run(self, paths = [], new = False):
