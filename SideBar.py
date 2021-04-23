fa24d26fb3f1917a4aa789abe2579392edb89daf
Steve Richter <steve@nerdbrain.com>
2021-04-23 03:53:22 -0400

    Fix for view.file_name() being None (#39)
    * Fix for view.file_name() being None
    
    * Continue to next loop iteration if view.file_name() is None

--- SideBar.py
+++ SideBar.py
@@ -214,6 +214,8 @@ class SideBarMoveCommand(SideBarCommand):
         for window in sublime.windows():
             for view in window.views():
                 filename = view.file_name()
+                if not filename:
+                    continue
                 if os.path.commonprefix([source, filename]) == source:
                     view.retarget(
                         os.path.join(destination, filename[len(source):])
@@ -225,7 +227,10 @@ class SideBarMoveCommand(SideBarCommand):
         destination = os.path.normcase(os.path.abspath(destination))
         for window in sublime.windows():
             for view in window.views():
-                path = os.path.abspath(view.file_name())
+                filename = view.file_name()
+                if not filename:
+                    continue
+                path = os.path.abspath(filename)
                 if os.path.normcase(path) == source:
                     view.retarget(destination)
 
